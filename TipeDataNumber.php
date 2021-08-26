e2ef223f5cbe22e1c06b94c07feee6749042f754
AdamZullowa06 <adamzullowa06@gmail.com>
2021-08-26 19:52:24 +0700

    new file TipeDataNumber

---
+++ TipeDataNumber.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(0123);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b11111111);
+
+echo "Underscore di Number : ";
+var_dump(1_234_567);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point dengan E notation Plus (1.2 x 1000) :";
+var_dump(1.2e3);
+
+echo "Floating Point dengan E notation Minus (7 x 0.001) :";
+var_dump(7e-3);
+
+echo "Underscore di Floating Point :";
+var_dump(1_234.567);
